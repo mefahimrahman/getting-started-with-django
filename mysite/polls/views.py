0789d8b2712dff5e4b8338337cecf01efaea206f
mefahimrahman <mefahimrahman@gmail.com>
2020-10-08 21:36:19 +0600

    Simplest view added
    Three views are added at polls/views.py. To understand the way of the
    url works in django.

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -4,3 +4,14 @@ from django.http import HttpResponse
 # Create your views here.
 def index(request):
     return HttpResponse("<h1>Hello world</h1><br><h3>You are at polls Index.</h3>")
+
+def detail(request, question_id):
+    return HttpResponse("You're looking at the results of question %s." % question_id)
+
+def results(request, question_id):
+    response = "You're looking at the results of question %s."
+    return HttpResponse(response % question_id)
+
+def vote(request, question_id):
+    return HttpResponse("You're voting on question %s." % question_id)
+
