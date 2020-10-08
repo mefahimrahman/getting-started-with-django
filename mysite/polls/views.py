4238534cb0449b42f06166ac4a9d0c945dd625e3
mefahimrahman <mefahimrahman@gmail.com>
2020-10-08 22:03:58 +0600

    Template added
    Template added on the poll app. Now calling template from view part.
    detail function is corrected and also index function in views.

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,15 +1,16 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Question
+from django.template import loader
 
 # Create your views here.
 def index(request):
-    return HttpResponse("<h1>Hello world</h1><br><h3>You are at polls Index.</h3>")
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    context = { 'latest_question_list': latest_question_list }
+    return render(request, 'polls/index.html', context)
 
 def detail(request, question_id):
-    latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    output = ', '.join([q.question_text for q in latest_question_list])
-    return HttpResponse(output)
+    return HttpResponse("You're looking at question %s." % question_id)
 
 def results(request, question_id):
     response = "You're looking at the results of question %s."
